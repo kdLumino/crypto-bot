bfb3dbd0abd45d716474c64f1a8c99ed59666b2b
kdLumino <kuldeep.luminoguru@gmail.com>
2018-12-31 16:13:39 +0530

    roll back last update code form add pay option for more markets

--- app/Http/Controllers/fbbotcontroller.php
+++ app/Http/Controllers/fbbotcontroller.php
@@ -15,8 +15,7 @@ class fbbotcontroller extends Controller
     	$marketsarr = $this->fetchMarketBaseQuote('Kraken');
         $payload = $data['entry'][0]['messaging'][0];
         $id      = $data["entry"][0]["messaging"][0]["sender"]["id"];
-        $kd = json_encode($payload);
-         file_put_contents("php://stderr", "$kd");
+  
         if( !empty($payload) ){
             if( !empty($payload['postback']['payload']) ){
                 if($payload['postback']['payload'] == 'get'){
@@ -34,6 +33,8 @@ class fbbotcontroller extends Controller
                 }else if($payload['message']['quick_reply']['payload'] == 'no_subscribe'){
                     $this->selectMarketMessage($id, $payload['message']['quick_reply']['payload']);
                 }else if( $payload['message']['quick_reply']['payload'] == 'start_default'){
+                          $kd = json_encode($payload);
+         file_put_contents("php://stderr", "$kd");
                     $this->defaultTextMessage($id, $payload['postback']['payload']);
                 }else{
                     $this->marketTextMessage($id, $payload['message']['quick_reply']['payload']);
