d6e7b5c09765e47eb8c903bcc4bcfffa185139e9
kdLumino <kuldeep.luminoguru@gmail.com>
2019-01-02 11:57:22 +0530

    update start flow quick reply

--- app/Http/Controllers/fbbotcontroller.php
+++ app/Http/Controllers/fbbotcontroller.php
@@ -15,7 +15,8 @@ class fbbotcontroller extends Controller
     	$marketsarr = $this->fetchMarketBaseQuote('Kraken');
         $payload = $data['entry'][0]['messaging'][0];
         $id      = $data["entry"][0]["messaging"][0]["sender"]["id"];
-  
+
+			  
         if( !empty($payload) ){
             if( !empty($payload['postback']['payload']) ){
                 if($payload['postback']['payload'] == 'get'){
@@ -31,9 +32,9 @@ class fbbotcontroller extends Controller
                 if($payload['message']['quick_reply']['payload'] == 'market_subscribe'){
                     $this->selectMarketMessage($id, $payload['message']['quick_reply']['payload']);
                 }else if($payload['message']['quick_reply']['payload'] == 'no_subscribe'){
-                    $this->selectMarketMessage($id, $payload['message']['quick_reply']['payload']);
+                    $this->defaultTextMessage($id, $payload['message']['quick_reply']['payload']);
                 }else if( $payload['message']['quick_reply']['payload'] == 'start_default'){
-                    $this->defaultTextMessage($id, $payload['postback']['payload']);
+					$this->defaultTextMessage($id, $payload['message']['quick_reply']['payload']);
                 }else{
                     $this->marketTextMessage($id, $payload['message']['quick_reply']['payload']);
                 }
@@ -114,49 +115,60 @@ class fbbotcontroller extends Controller
         curl_close($ch);
     }
     private function defaultTextMessage($recipientId, $messageText){
-                                           file_put_contents("php://stderr", "last");
-               file_put_contents("php://stderr", "$messageText   ");
-    	$this->sendAction($recipientId);
-    
+									
+		file_put_contents( "php://stderr","$messageText");
+		file_put_contents( "php://stderr","start default 1");
+	
+		$this->sendAction($recipientId);
+		
+		file_put_contents( "php://stderr","start default 2");
+
         $user = $this->getUserDetails($recipientId);
 		$userdata = json_decode($user);
-		
+		$subscribe = SubscribeMarket::where('user_id', $recipientId)->get()->toArray();
+		$temparray = [];
+		if($subscribe){
+			$temp['type'] = 'postback';
+			$temp['title'] = 'See Your Markets!';
+			$temp['payload'] = 'subscribe_list';
+			array_push($temparray,$temp);
+		}
+		$temp['type'] = 'postback';
+		$temp['title'] = 'Pick Our Exchanges!';
+		$temp['payload'] = 'get_exchange';
+		array_push($temparray,$temp);
+
+		file_put_contents( "php://stderr","start default 3");
     	$url = 'https://graph.facebook.com/v3.2/me/messages?access_token=' . env("PAGE_ACCESS_TOKEN");
 		    /*initialize curl*/
 		    $ch = curl_init($url);
-	       		       /*prepare response*/
-			    $jsonData = '{
-			    "recipient":{
-			        "id":"' . $recipientId . '"
-			        },
-			        "message":{
-				    "attachment":{
-				      "type":"template",
-				      "payload":{
-				        "template_type":"generic",
-				        "elements":[
-				           {
-				            "title":"Hey ' . $userdata->first_name . ' Good To see You.!",
-				            "image_url":"https://lz-bot.herokuapp.com/image/bitcoin-falling-760x400.jpg",
-				            "subtitle":"We have the right hat for everyone.",
-				            "default_action": {
-				              "type": "web_url",
-				              "url": "https://lz-bot.herokuapp.com",
-				              "webview_height_ratio": "tall",
-				            },
-				            "buttons":[
-				              {
-				                "type":"postback",
-				                "title":"Pick Our Exchanges!",
-				                "payload":"get_exchange"
-				              }              
-				            ]      
-				          }
-				        ]
-				      }
-				    }
-				  }
-			    }';
+				/*prepare response*/
+				$jsonData = '{
+				"recipient":{
+					"id":"' . $recipientId . '"
+				},
+				"message":{
+				"attachment":{
+					"type":"template",
+					"payload":{
+					"template_type":"generic",
+					"elements":[
+						{
+						"title":"Hey ' . $userdata->first_name . ' Good To see You.!",
+						"image_url":"https://lz-bot.herokuapp.com/image/bitcoin-falling-760x400.jpg",
+						"subtitle":"We have the right hat for everyone.",
+						"default_action": {
+							"type": "web_url",
+							"url": "https://lz-bot.herokuapp.com",
+							"webview_height_ratio": "tall",
+						},
+						"buttons": '.json_encode( $temparray ).'
+						}
+					]
+					}
+				}
+				}
+			}';
 	       
 	        /* curl setting to send a json post data */
 		    curl_setopt($ch, CURLOPT_POST, 1);
@@ -192,7 +204,8 @@ class fbbotcontroller extends Controller
 					array_push($temparray,$temp);
 					}
 				}
-			     $jsonData = '{
+			
+			    $jsonData = '{
 					    "recipient":{
 					        "id":"' . $recipientId . '"
 					        },
@@ -461,12 +474,12 @@ class fbbotcontroller extends Controller
                     "id":"' . $recipientId . '"
                     },
                     "message":{
-                        "text": "Thanks for Subscribe Our Market. We will Notify You When be Get SELL/BUY Signal! You have only subscribe three (3) markets in free version. if you want subscribe more markets apply for paid version!",
+                        "text": "Thanks for Subscribe Our Market. We will Notify You When be Get SELL/BUY Signal!. if you want subscribe more markets apply for paid version!",
                        "quick_replies": [
 							    	{
 							    		"content_type": "text",
-							    		"title": "YES",
-							    		"payload": "market_subscribe",
+							    		"title": "PAID",
+							    		"payload": "paid_version",
 							    		"image_url": "https://via.placeholder.com/150"
 							    	},
 							    	{
@@ -484,15 +497,6 @@ class fbbotcontroller extends Controller
 		    	Cache::pull('marketBaseId');
 		    	Cache::pull('marketBaselastPrice');
 		
-			}else{
-			   $jsonData = '{
-				    "recipient":{
-				        "id":"' . $recipientId . '"
-				        },
-				        "message":{
-						    "text": "Thanks For Connecting Us!",
-						}
-				    }';
 			}
 	        /* curl setting to send a json post data */
 		    curl_setopt($ch, CURLOPT_POST, 1);
@@ -504,8 +508,12 @@ class fbbotcontroller extends Controller
     }
     private function sendWelcomeMessage($recipientId, $messageText)
     {	
-    	 $this->sendAction($recipientId);
-    	 Cache::pull('marketBaseQuote');
+		$this->sendAction($recipientId);
+		 
+    	Cache::pull('marketBaseQuote');
+		Cache::pull('marketExchangeId');
+		Cache::pull('marketBaseId');
+		Cache::pull('marketBaselastPrice');
          // set gretting text array
     	 $grettingtext = array("HI", "HELLO", "HEY", "GET", "START");
     	 //Get user details based on recipient id (nmae,profile,location, etc)
