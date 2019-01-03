7a0fb0dec0c1e614d66bddec707b2cf8dae3c0cd
kdLumino <kuldeep.luminoguru@gmail.com>
2019-01-03 10:38:34 +0530

    update image path

--- app/Http/Controllers/test.php
+++ app/Http/Controllers/test.php
@@ -12,9 +12,9 @@ class test extends Controller
 {
     
     public function test(){
-
+      
         $max_sub_mrkt =  Config::get('markets.sub_market_number');
-         
+         dd( url('/').'/image/call.jpg');
        $count = SubscribeMarket::where([['user_id','=', '2950844664941572'],['market_symbol','=', 'XMR/USD']])->count();
         dd($count);
         foreach ($kd as $key => $value) {
